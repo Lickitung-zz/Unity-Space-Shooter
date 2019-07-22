4062f1a3b65d7e95a3f58c6b82d9c866b0663cf0
SIeep <brandon_n_evans@live.com>
2019-07-22 12:48:29 -0500

    Add enemy spawning interval
    Add enemy spawning interval

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //spawn game object every 5 seconds
+    //Create a coroutine of type IEnumerator -- Yield Events
+    //while loop
+    IEnumerator SpawnRoutine()
+    {
+        Enemy enemy = transform.GetComponent<Enemy>();
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-18.0f, 12.5f), 12.48f, 0);
+            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(5);
+        }
     }
 }
